002e177fb5586878f3d077c64e47a63a9229b1a8
Ethan-Maridat <ethanmrdt@gmail.com>
2024-03-27 08:48:38 +0100

    feat: init project

---
+++ projettodos/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <QuizVue :quizData="quizData" />
+  </div>
+</template>
+
+<script>
+import QuizVue from "./components/quiz.vue";
+
+export default {
+  components: {
+    QuizVue,
+  },
+  data() {
+    return {
+      quizData: [
+        {
+          question: "Quelle est la capitale de la France?",
+          choices: ["Paris", "Londres", "Madrid"],
+          answer: "Paris",
+        },
+        {
+          question: "Quel est le plus haut sommet du monde?",
+          choices: ["Mont Everest", "K2", "Makalu"],
+          answer: "Mont Everest",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ projettodos/src/components/quiz.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h2>Quiz Questions</h2>
+      <ul>
+        <li v-for="quiz in quizData" :key="quiz.question">
+          <h3>{{ quiz.question }}</h3>
+          <ul>
+            <li v-for="choice in quiz.choices" :key="choice">{{ choice }}</li>
+          </ul>
+          <p>Answer: {{ quiz.answer }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      quizData: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: #42b883;
+  }
+  </style>
